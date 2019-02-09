0a1ab35243c14f0842a9e4fb3075936516ceced4
paoladlp92 <pdlpgzz@gmail.com>
2019-02-08 21:12:28 -0600

    navbars chidas

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
-        msg: "Joy Spark!",
+        msg: "",
         examples: dbExamples
       });
     });
